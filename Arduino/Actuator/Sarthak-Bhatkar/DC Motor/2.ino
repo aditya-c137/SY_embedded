b199c17a0ee659da4f817a1802a7c017aa2b2816
Sarthak Bhatkar <59932038+sarthakbhatkar1@users.noreply.github.com>
2021-01-01 00:58:08 +0530

    Update 2.ino

--- Arduino/Actuator/Sarthak-Bhatkar/DC Motor/2.ino
+++ Arduino/Actuator/Sarthak-Bhatkar/DC Motor/2.ino
@@ -1,1 +1,21 @@
+/*		
+* Read the innput from Serial Moniter
+* to control the speed of the DC motor.
+*/
 
+int motorPin = 6;
+int read;
+
+void setup()
+{
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  while(Serial.available())
+  	read = Serial.parseInt();
+  	if(read>0&&read<255)
+  		analogWrite(motorPin, read);
+}
