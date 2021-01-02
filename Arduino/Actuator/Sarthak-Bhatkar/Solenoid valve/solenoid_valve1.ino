9336cdbc89be2872ecd3c022a3498bc2b1a98939
Sarthak Bhatkar <59932038+sarthakbhatkar1@users.noreply.github.com>
2021-01-02 16:54:29 +0530

    Create solenoid_valve1.ino

---
+++ Arduino/Actuator/Sarthak-Bhatkar/Solenoid valve/solenoid_valve1.ino
@@ -0,0 +1,43 @@
+
+/*
+* In the circuit, two push buttons are used. 
+* Using those two, Solenoid can be turn ON or OFF.
+*/
+
+#define pushButtonON 9		// To turn 'ON' the Solenoid
+#define pushButtonOFF 10	// To turn OFF the Solenoid
+#define solenoidValve 5		
+bool flag = false;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pushButtonON, INPUT);
+  pinMode(pushButtonOFF, INPUT);
+  pinMode(solenoidValve, OUTPUT);
+  while(!Serial)
+  Serial.println("Solenoid can be turn 'ON' and 'OFF' using the push buttons.");
+}
+
+void loop()
+{
+  //digitalWrite(solenoidValve, HIGH);
+  if(digitalRead(pushButtonON)==HIGH)
+  {
+    digitalWrite(solenoidValve, HIGH);
+    Serial.println("Solenoid -> ON.");
+    delay(500);
+  }
+  else if(digitalRead(pushButtonOFF)==HIGH)
+  {
+    digitalWrite(solenoidValve, LOW);
+    Serial.println("Solenoid -> OFF.");
+    delay(500);
+  }
+  
+  while(!flag)
+  {
+    Serial.println("Solenoid -> OFF.");
+  	flag = true;
+  }
+  
+}
