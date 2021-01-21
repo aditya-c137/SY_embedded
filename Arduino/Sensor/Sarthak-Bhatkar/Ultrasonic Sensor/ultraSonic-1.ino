6736e8aee6578371bf7c52b2a73bbe8f54d81e79
Sarthak Bhatkar <59932038+sarthakbhatkar1@users.noreply.github.com>
2021-01-21 22:53:02 +0530

    Update ultraSonic-1.ino

--- Arduino/Sensor/Sarthak-Bhatkar/Ultrasonic Sensor/ultraSonic-1.ino
+++ Arduino/Sensor/Sarthak-Bhatkar/Ultrasonic Sensor/ultraSonic-1.ino
@@ -19,7 +19,7 @@ void loop()
 	delayMicroseconds(2);
 	digitalWrite(trigPin, HIGH);
 	delayMicroseconds(10);
-	//digitalWrite(trigPin, LOW);
+	digitalWrite(trigPin, LOW);
 	duration = pulseIn(echoPin, HIGH);
 	distance= duration*0.034/2;
 	Serial.print("Distance: ");
